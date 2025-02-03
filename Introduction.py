d4cf28327474e702f8a105a8add4026092911ab5
aliandreckinthe254 <iantekiba@gmail.com>
2025-02-03 16:06:22 +0300

    initial

---
+++ Introduction.py
@@ -0,0 +1,7 @@
+#Print is indeed a method used to display a value
+
+
+print("Claire")
+print("eMobilis")
+print("100")
+print(10+20)
---
+++ Variable.py
@@ -0,0 +1,16 @@
+#Variable is a container used to store a value
+location ="Westlands"
+firstname="Claire"
+lastname="Githii"
+school="eMobilis"
+print(location)
+print(firstname)
+print(lastname)
+print(school)
+print(firstname,lastname)
+print("My fullname is ",firstname,lastname)
+print(school,"Is a coding school")
+x=39
+u=2
+print(x+u)
+print("The product of 39 and 2 is" ,x*u)
